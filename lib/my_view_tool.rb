c81417921d6472b0d58bcc66f420e45290abca63
Taimoor Hassan <tempetationx@hotmail.com>
2019-03-07 12:27:07 +0500

    initial commit

---
+++ lib/my_view_tool.rb
@@ -0,0 +1,7 @@
+require "my_view_tool/version"
+require "my_view_tool/renderer"
+
+module MyViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/my_view_tool/version.rb
@@ -0,0 +1,3 @@
+module MyViewTool
+  VERSION = "0.1.0"
+end
